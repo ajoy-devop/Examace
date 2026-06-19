d7873c881b28557e74f25bc693a3d55c0c3dcbfc
ajoy-devop <ajoymick@gmail.com>
2026-06-19 14:49:40 +0530

    Update NotFoundPage.jsx

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Zap } from 'lucide-react';
 import Button from '../components/ui/Button';
 
 export default function NotFoundPage() {
